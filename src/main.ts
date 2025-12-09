ff041cb66f0f52fa91d6895fb7816cb0e91b1856
Ivan Ilichev <ilichev-ivn@yandex.ru>
2025-12-09 22:50:35 +0300

    add phaser.js

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hello vite + typescript`;
+import Phaser from 'phaser';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    create: function () {
+      this.add.text(100, 100, 'Hello, Phaser in TypeScript!', { color: 'yellow' });
+    },
+  },
+};
+
+const game = new Phaser.Game(config);
