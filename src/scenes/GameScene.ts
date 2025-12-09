60dc6b71bff9a21f8d9b2da3915ac484704ed705
Ivan Ilichev <ilichev-ivn@yandex.ru>
2025-12-10 00:27:57 +0300

    add game field

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -10,14 +10,24 @@ export class GameScene extends Phaser.Scene {
     this.load.image('player', './assets/player.png');
     this.load.image('bot', './assets/bot.png');
     this.load.image('ball', './assets/ball.png');
+    this.load.image('game-field', './assets/game-field.png');
+    this.load.image('goal-area', './assets/goal-area.png');
   }
 
   create() {
     // game field
-    this.add.text(0, 0, 'PHASER', { color: 'yellow', backgroundColor: 'green' });
-    this.add.text(150, 0, 'TypeScript', { color: 'red', fontStyle: 'italic' });
-    this.add.text(300, 0, 'Any Sponsor');
+    this.physics.add.sprite(400, 300, 'game-field');
 
+    // ads
+    this.add.text(20, 570, 'PHASER', { color: 'yellow', backgroundColor: 'green' });
+    this.add.text(150, 570, 'TypeScript', { color: 'red', fontStyle: 'italic' });
+    this.add.text(300, 570, 'Any Sponsor');
+
+    // goal areas
+    this.physics.add.sprite(100, 300, 'goal-area');
+    this.physics.add.sprite(700, 300, 'goal-area');
+
+    // ball
     this.physics.add.sprite(400, 300, 'ball');
 
     // players
