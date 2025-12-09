7b7412051791e4b7816aac17e4862dc5f36f967e
Ivan Ilichev <ilichev-ivn@yandex.ru>
2025-12-09 23:22:04 +0300

    add sprites

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+  },
   scene: [GameScene],
 };
 
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -7,11 +7,22 @@ export class GameScene extends Phaser.Scene {
 
   preload() {
     // Загрузка спрайтов и ресурсов
+    this.load.image('player', './assets/player.png');
+    this.load.image('bot', './assets/bot.png');
+    this.load.image('ball', './assets/ball.png');
   }
 
   create() {
-    this.add.text(400, 300, 'Hello, Phaser in TypeScript!', { color: 'yellow' });
-    // Инициализация начальных объектов
+    // game field
+    this.add.text(0, 0, 'PHASER', { color: 'yellow', backgroundColor: 'green' });
+    this.add.text(150, 0, 'TypeScript', { color: 'red', fontStyle: 'italic' });
+    this.add.text(300, 0, 'Any Sponsor');
+
+    this.physics.add.sprite(400, 300, 'ball');
+
+    // players
+    this.physics.add.sprite(200, 300, 'player');
+    this.physics.add.sprite(600, 300, 'bot');
   }
 
   update() {
