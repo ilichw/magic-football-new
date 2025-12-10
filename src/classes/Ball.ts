ef5e2afb481c359dc1abea6fc2c8705914177912
Ivan Ilichev <ilichev-ivn@yandex.ru>
2025-12-10 09:10:07 +0300

    fix ball initial velocity

--- src/classes/Ball.ts
+++ src/classes/Ball.ts
@@ -15,8 +15,8 @@ export class Ball extends Phaser.Physics.Arcade.Sprite {
 
   setRandomVelocity(speed: number) {
     // Генерация случайного угла в радианах
-    const coef = Math.round(Math.random());
-    const angle = coef ? Phaser.Math.Between(120, 240) : Phaser.Math.Between(-60, 60);
+    const coef = Math.round(Math.random() * 3);
+    const angle = Phaser.Math.Between(30 + 90 * coef, 60 + 90 * coef);
     const angleRad = Phaser.Math.DegToRad(angle);
 
     // Вычисление проекций скорости
